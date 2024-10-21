2e9658b109c2eae54c45a2b683e30866c3d0a0a4
jihwooon <jihwooon@gmail.com>
2024-10-21 21:49:50 +0900

    Cargo 클래스 내에 Receiver 클래스를 추가하라
    주요 변경사항:
    1. 화물 예약에 주문자 정보를 담는 값 객체를 추가했습니다.

--- src/main/java/com/example/demo/controllers/domain/Cargo.java
+++ src/main/java/com/example/demo/controllers/domain/Cargo.java
@@ -3,4 +3,6 @@ package com.example.demo.controllers.domain;
 public class Cargo {
 
     private CargoId id;
+
+    private Receiver receiver;
 }
---
+++ src/main/java/com/example/demo/controllers/domain/Receiver.java
@@ -0,0 +1,7 @@
+package com.example.demo.controllers.domain;
+
+public class Receiver {
+
+    private String name;
+    private String phone;
+}
