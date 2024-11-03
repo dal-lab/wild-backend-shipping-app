9a44d575b485fde2dd6c55390bddc5e7639af332
ramyo564 <yohan032yohan@gmail.com>
2024-11-03 16:45:12 +0900

    Cargo - RootEntity (CargoAggregate) - 도메인 로직 (배송상태변경하기)

--- src/main/java/com/example/demo/model/Cargo.java
+++ src/main/java/com/example/demo/model/Cargo.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 
 import com.example.demo.controllers.dto.CargoCreateRequestDto;
 import com.example.demo.controllers.dto.CargoResponseDto;
+import com.example.demo.controllers.dto.CargoUpdateDto;
 import com.example.demo.exception.InvalidTrackingIdException;
 import com.example.demo.infrastructure.CargoRepository;
 import org.springframework.stereotype.Component;
@@ -32,6 +33,18 @@ public class Cargo {
     public CargoResponseDto getCargo(String trackingId) {
         CargoItem cargoItem = getCargoItem(trackingId);
         return getCargoResponseDto(cargoItem);
+    }
+
+    // 화물 배송 상태 변경
+    public CargoResponseDto updateCargo(
+            String trackingId,
+            CargoUpdateDto cargoUpdateDto
+    ) {
+        CargoItem updateCargo = updateCargoItem(trackingId,
+                cargoUpdateDto);
+        cargoRepository.save(updateCargo);
+
+        return getCargoResponseDto(updateCargo);
     }
 
     private CargoResponseDto getCargoResponseDto(CargoItem cargoItem) {
@@ -70,6 +83,24 @@ public class Cargo {
                 cargoCreateRequestDto.endLocation(),
                 null,
                 new Status("배송 대기중", CurrentStatus.PENDING)
+        );
+    }
+
+    private CargoItem updateCargoItem(String trackingId,
+            CargoUpdateDto cargoUpdateDto) {
+        CargoItem cargoItem = getCargoItem(trackingId);
+        return new CargoItem(
+                cargoItem.getId(),
+                cargoItem.getName(),
+                cargoItem.getDescription(),
+                cargoItem.getQuantity(),
+                cargoItem.getWeight(),
+                cargoItem.getLength(),
+                cargoItem.getHeight(),
+                cargoItem.getStartLocation(),
+                cargoItem.getEndLocation(),
+                cargoUpdateDto.currentLocation(),
+                cargoUpdateDto.status()
         );
     }
 
