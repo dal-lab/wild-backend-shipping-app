b4767c4cf40a02f3143532fcec61ca49516df9ee
ramyo564 <yohan032yohan@gmail.com>
2024-11-03 16:20:42 +0900

    CargoController - create 기능 구현

---
+++ src/main/java/com/example/demo/controllers/CargoController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controllers;
+
+import com.example.demo.controllers.dto.CargoCreateRequestDto;
+import com.example.demo.controllers.dto.CargoResponseDto;
+import com.example.demo.service.CargoService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cargoes")
+public class CargoController {
+
+    private final CargoService cargoService;
+
+    public CargoController(CargoService cargoService) {
+        this.cargoService = cargoService;
+    }
+
+    @PostMapping
+    public CargoResponseDto createCargo(
+            @RequestBody
+            @Valid CargoCreateRequestDto cargoCreateRequestDto
+    ) {
+        return cargoService.createCargo(cargoCreateRequestDto);
+    }
+}
